b9b7b23cc651aa9dd12ca5db9c4fb0cf48002a52
waqas996 <waqas996@gmail.com>
2023-02-23 21:19:15 +0500

    To fix issue when sometimes Guzzle files were not autoloaded due to duplicated hash value

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,9 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
-    'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
-    'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
-    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '9489e01daf1c4fea15191b1fa66ae1d5' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     '42f29e5b17a2fb90587482aa5dc87da1' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     '174f0c49b6b88969abdb283fd27db628' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
